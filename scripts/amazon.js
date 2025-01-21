6d66aeaa1ac37cca8330634d6867fdc4f92e487f
PC <hoangtuan0796991061@gmail.com>
2025-01-21 19:50:00 +0700

    make the cart quantity button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,7 +76,22 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=> {
                 quantity: 1
             });
         }
+
+        let cartQuantity = 0
+
+        cart.forEach((item)=>{
+            cartQuantity += item.quantity
+        })
+        // using dom to make the cart quantity change when we click add to cart button
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+        console.log(cartQuantity);
         console.log(cart);
     });
 });
+
+
+
+
+
 // Main idea of Javascript, we use it to save data, generate HTML, and make it interactive.
