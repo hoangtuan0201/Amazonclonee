8e8a705a81cfc74a8e2d0e0ade0f6ad6ce6f9fd2
PC <hoangtuan0796991061@gmail.com>
2025-02-05 20:55:11 +0700

    make delete button in checkout.js interactively

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,8 @@ cart.forEach((cartItem) => {
 
     // Append the checkout item HTML structure
     checkoutHTML += `    
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
         <div class="cart-item-details-grid">
@@ -80,5 +81,10 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
-    })
-})
+
+        const container = document.querySelector(
+            `.js-cart-item-container-${productId}`
+        );
+        container.remove();
+    });
+});
